2710616da4b89fb91e737ef6d6bc70faea8ef811
Thanakorn <Thanakorn.pleum30571@mail.kmutt.ac.th>
2022-09-30 10:20:31 +0700

    Solved bug safe argh

--- News2/app/src/main/java/com/siamsquared/news2/ui/activity/MainActivity.kt
+++ News2/app/src/main/java/com/siamsquared/news2/ui/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.siamsquared.news2.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.siamsquared.news2.R
 import com.siamsquared.news2.databinding.ActivityMainBinding
 
@@ -14,6 +17,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.NavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
 
     }
 }
