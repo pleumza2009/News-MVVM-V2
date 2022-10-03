dedb7ca8cac52e7915467d9be5a77cd0c8afd28d
Thanakorn <Thanakorn.pleum30571@mail.kmutt.ac.th>
2022-10-03 21:28:54 +0700

    setyp room

---
+++ News2/app/src/main/java/com/siamsquared/news2/db/ArticleDao.kt
@@ -0,0 +1,22 @@
+package com.siamsquared.news2.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.siamsquared.news2.model.Article
+
+interface ArticleDao {
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
