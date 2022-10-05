80cdabc1c9df70f0168d48fa59d7f368c079a593
Thanakorn <Thanakorn.pleum30571@mail.kmutt.ac.th>
2022-10-05 22:19:55 +0700

    Adjust previous commit

--- News2/app/src/main/java/com/siamsquared/news2/db/ArticleDao.kt
+++ News2/app/src/main/java/com/siamsquared/news2/db/ArticleDao.kt
@@ -1,12 +1,14 @@
 package com.siamsquared.news2.db
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.siamsquared.news2.model.Article
 
+@Dao
 interface ArticleDao {
 
 
